94fc6dd5d62a7bc1d1c68954997787bf7c1fa143
JonathanVeraMarin98 <jonathanveramarin98@outlook.com>
2026-03-11 20:07:51 -0500

    Update getGifs.js

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async(category) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=9K9nPcBWqUfFqqYZb2wOde80X4Xda8H2&q=${ category }&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=#&q=${ category }&limit=10`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
